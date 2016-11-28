b76c639d0d47313e481df0df4ba49a3006a75497
shiguang15 <shiguang3@jd.com>
2016-11-28 18:38:17 +0800

    修改错字

--- src/main/java/designpatterns/creationalpatterns/singleton/Ehan.java
+++ src/main/java/designpatterns/creationalpatterns/singleton/Ehan.java
@@ -7,7 +7,7 @@ package designpatterns.creationalpatterns.singleton;
  * 例如初始化的对象包含特别多的数据占用大量内存，但是一直没有地方调用{@link #getInstance()}方法。
  * 缺点2：
  * 当创建实例依赖于一些其他操作时，这种方式很不方便，
- * 将依赖的操作例如读取配置文件，放到构造函数里，是一种不太还的解决方案。
+ * 将依赖的操作例如读取配置文件，放到构造函数里，是一种不太好的解决方案。
  * Created by shiguang3 on 2016/11/28.
  */
 public class Ehan {
